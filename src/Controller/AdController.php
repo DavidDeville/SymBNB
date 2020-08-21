b9a7a12ed44516c6722a448fe4a7d2b4e8a50d97
david <david.deville59@gmail.com>
2020-08-22 01:29:46 +0200

    Minor bug fixed. Added a form for the user to send his own Ad and flashes display if there's an error. Navbar and flashes were moved to another twig template

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,9 +5,10 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -31,10 +32,28 @@ class AdController extends AbstractController
      * 
      * @return Response
      */
-    public function create() {
+    public function create(Request $request, EntityManagerInterface $manager) {
         $ad = new Ad();
 
         $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            //$manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <stong>{$ad->getTitle()}</strong> a bien été enregistrée!"
+            );
+
+            return $this->redirectToRoute("ads_show", [
+                'slug' => $ad->getSlug()
+            ]);
+        }
         
         return $this->render("ad/new.html.twig", [
             'form' => $form->createView()
